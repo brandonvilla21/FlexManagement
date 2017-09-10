8451dbf9807db0aa96f255f813b06fe8287822bb
marcos8896 <elyomarcos@gmail.com>
2017-09-10 13:43:42 -0600

    Added employee-edit and employee-show components.

---
+++ src/app/employee/components/employee-show/employee-show.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-show',
+  templateUrl: './employee-show.component.html',
+  styleUrls: ['./employee-show.component.scss']
+})
+export class EmployeeShowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

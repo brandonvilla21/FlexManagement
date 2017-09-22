938d335bfe9706d94b5f6676e0f9c2cb00a23603
marcos8896 <elyomarcos@gmail.com>
2017-09-21 20:47:07 -0600

    Added full functionality for sale-show component.

--- src/app/processes/sale/components/sale-show/sale-show.component.ts
+++ src/app/processes/sale/components/sale-show/sale-show.component.ts
@@ -1,18 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Employee } from './../../../../employee/employee.model';
-import { Customer } from './../../../../customer/customer.model';
-import { Product } from './../../../../product/product.model';
-import { EmployeeService } from './../../../../employee/services/employee/employee.service';
-import { CustomerService } from './../../../../customer/services/customer/customer.service';
-import { ProductService } from './../../../../product/services/product.service';
+import { DatePipe } from '@angular/common';
 import { Observable } from 'rxjs/Observable';
-// import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-// import { DialogService } from 'ng2-bootstrap-modal';
-import { ProductSaleProductInterface } from '../../models/product-sale-product.model';
-import { SearchModalComponent } from './../../../../shared/search-modal/search-modal.component';
 import { SaleProductInterface } from '../../models/sale-product.model';
 import { SaleProductService } from './../../services/sale-product.service';
+// import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-sale-show',
@@ -21,42 +15,27 @@ import { SaleProductService } from './../../services/sale-product.service';
 })
 export class SaleShowComponent implements OnInit {
 
-  public product: Product;
-  public employee: Employee;
-  public customer: Customer;
+  public saleProductId: string;
   public saleProduct: SaleProductInterface;
 
   constructor(
-    private productService: ProductService,
-    private employeeService: EmployeeService,
-    private customerService: CustomerService,
-    private saleProductService: SaleProductService
+    private saleProductService: SaleProductService,
+    // private location: Location,
+    private activatedRoute: ActivatedRoute
    ){ }
 
   ngOnInit() {
-    // this.getSale();
+    this.getSale();
   }
 
   getSale() {
-    this.saleProductService.findById('1').subscribe( saleProduct => {
-      // this.saleProduct = saleProduct
-      console.log(saleProduct);
-    } );
+    this.activatedRoute.params.subscribe(parameters => {
+      this.saleProductId = parameters['id'];
+
+      this.saleProductService.findByIdJoin('1').subscribe(saleProduct => {
+        this.saleProduct = saleProduct;
+        console.log('saleProduct: ', saleProduct);
+      });
+    });
   }
-
-  // getProduct() {
-  //   this.productService.all()
-  //     .subscribe( product => this.product = product );
-  // }
-
-  // getEmployee() {
-  //   this.employeeService.all()
-  //     .subscribe( employee => this.employee = employee );
-  // }
-
-  // getCustomer() {
-  //   this.customerService.all()
-  //     .subscribe( customer => this.customer = customer );
-  // }
-
 }

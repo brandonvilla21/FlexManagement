38d923b4b259ce8dee3ec166c4ff471f0141df5a
Brandon Villa <bornofos@gmail.com>
2017-10-17 23:06:53 -0500

    Provider & Employee reports finished

--- src/app/reports/components/employee-report/employee-report.component.ts
+++ src/app/reports/components/employee-report/employee-report.component.ts
@@ -1,3 +1,6 @@
+import { Employee } from './../../../employee/employee.model';
+import { Ng2PdfService } from './../../../shared/ng2-pdf/ng2-pdf.service';
+import { ReportsService } from './../../services/reports.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-report.component.scss']
 })
 export class EmployeeReportComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public employees: Employee[];
+  constructor(
+    private ng2PdfService: Ng2PdfService,
+    private reportsService: ReportsService
+  ) {
+    this.employees = [];
   }
 
+  ngOnInit() {
+    this.getTableData();
+  }
+
+  getTableData() {
+    this.reportsService.getTabeData( 'employee ')
+      .subscribe( res => this.employees = res );
+  }
+
+  download() {
+
+    const columns = [ 'ID', 'NOMBRE', 'APELLIDO(S)', 'DIRECCIÓN', 'WHATSAPP'];
+    const rows = [];
+    this.employees.forEach( employee => {
+      rows.push([
+        employee.employee_id,
+        employee.name || '',
+        employee.lastname || '',
+        employee.address || '',
+        employee.whatsapp || '',
+      ])
+    })
+
+    // Set the date to PDF
+    const date = 'Fecha: ' + new Date().toLocaleDateString();
+
+    this.ng2PdfService.pdfTableDate( columns, rows, 'LISTA DE EMPLEADOS', 'Empleados.pdf', date );
+  }
 }

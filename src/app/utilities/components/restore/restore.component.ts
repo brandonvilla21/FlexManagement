0af0981b376f60456be9ff27eff78a8a86d9adc4
marcos8896 <elyomarcos@gmail.com>
2017-11-03 01:32:27 -0600

    Added credentials to the headers.

--- src/app/utilities/components/restore/restore.component.ts
+++ src/app/utilities/components/restore/restore.component.ts
@@ -1,3 +1,4 @@
+import { Headers } from '@angular/http';
 import { ConfigUrlService } from './../../../services/config-url/config.url.service';
 import { Component, OnInit } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
@@ -9,15 +10,29 @@ import { FileUploader } from 'ng2-file-upload';
 })
 export class RestoreComponent implements OnInit {
   public endPoint: string;
-  public uploader:FileUploader;
-  public db = {};
+  public uploader: FileUploader;
+  public db = { username: '', password: '' };
+  public uploaderOptions = {url: '', headers: []};
 
-  constructor( private configUrlService: ConfigUrlService ) { 
+  constructor(private configUrlService: ConfigUrlService) {
     this.endPoint = `${configUrlService.getBaseUrl()}/utilities/restore`;
-    this.uploader = new FileUploader({url: this.endPoint });
+    this.uploader = new FileUploader(this.uploaderOptions);
+    // Add in the other upload form parameters.        
+
   }
 
   ngOnInit() {
   }
+
+  addCredentialsBeforeUpload(item) {
+    this.uploaderOptions.url = this.endPoint;
+    this.uploaderOptions.headers = [];
+    this.uploaderOptions.headers.push({ name: 'username', value: this.db.username });
+    this.uploaderOptions.headers.push({ name: 'pass', value: this.db.password });
+    this.uploader.setOptions(this.uploaderOptions);
+    item.upload();
+  }
+
+
 
 }

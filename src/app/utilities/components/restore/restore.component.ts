8838c838c42aa854b0810bd3c153e883e247d46f
Marcos Barrera <elyomarcos@gmail.com>
2017-11-04 15:33:45 -0600

    Added proper handling for the message on the response from the server.

--- src/app/utilities/components/restore/restore.component.ts
+++ src/app/utilities/components/restore/restore.component.ts
@@ -13,24 +13,40 @@ export class RestoreComponent implements OnInit {
   public uploader: FileUploader;
   public db = { username: '', password: '' };
   public uploaderOptions = {url: '', headers: []};
+  public message;
 
   constructor(private configUrlService: ConfigUrlService) {
     this.endPoint = `${configUrlService.getBaseUrl()}/utilities/restore`;
     this.uploader = new FileUploader(this.uploaderOptions);
-    // Add in the other upload form parameters.        
 
   }
 
   ngOnInit() {
+    this.uploader.onCompleteItem = (item:any, response:any, status:any, headers:any) => {
+      this.message = JSON.parse(response);
+      this.message.status = status;
+      
+      console.log('headers: ', headers);
+      console.log('item: ', item);
+      console.log('status: ', status);
+      console.log("item uploaded", response);
+    };
   }
 
   addCredentialsBeforeUpload(item) {
+
+    //Cleaning the message object
+    this.message = {};
+
+
     this.uploaderOptions.url = this.endPoint;
     this.uploaderOptions.headers = [];
     this.uploaderOptions.headers.push({ name: 'username', value: this.db.username });
     this.uploaderOptions.headers.push({ name: 'pass', value: this.db.password });
     this.uploader.setOptions(this.uploaderOptions);
+    console.log("this.uploader.isUploading", this.uploader.isUploading);
     item.upload();
+
   }
 
 

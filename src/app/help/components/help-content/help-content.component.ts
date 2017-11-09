f38144d1d532eda1513980642f04eb5c82885c03
Brandon Villa <bornofos@gmail.com>
2017-11-09 13:23:35 -0600

    Help content

--- src/app/help/components/help-content/help-content.component.ts
+++ src/app/help/components/help-content/help-content.component.ts
@@ -9,6 +9,7 @@ export class HelpContentComponent implements    OnInit {
   public productos1 = 'http://localhost:3000/assets/productos1.png';
   public productos2 = 'http://localhost:3000/assets/productos2.png';
   public productos3 = 'http://localhost:3000/assets/productos3.png';
+  public productos4 = 'http://localhost:3000/assets/productos4.png';
   constructor() {
   }
 

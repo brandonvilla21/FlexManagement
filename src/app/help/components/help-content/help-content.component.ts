de5d31175d6f2bc2394ea3e14a2b1bca85765326
Brandon Villa <bornofos@gmail.com>
2017-11-09 21:29:17 -0600

    movimiento text finished

--- src/app/help/components/help-content/help-content.component.ts
+++ src/app/help/components/help-content/help-content.component.ts
@@ -14,14 +14,22 @@ export class HelpContentComponent implements    OnInit {
   public productos5: string
   public ventas1: string
   public ventas2: string
+  public ventas3: string
+  public ventas4: string
+  public ventas5: string
+  public ventas6: string
   constructor( configUrlService: ConfigUrlService) {
     this.productos1 = `${configUrlService.getBaseUrl()}/assets/productos1.png`
     this.productos2 = `${configUrlService.getBaseUrl()}/assets/productos2.png`
     this.productos3 = `${configUrlService.getBaseUrl()}/assets/productos3.png`
     this.productos4 = `${configUrlService.getBaseUrl()}/assets/productos4.png`
     this.productos5 = `${configUrlService.getBaseUrl()}/assets/productos5.png`
-    this.ventas1 = `${configUrlService.getBaseUrl()}/assets/ventas1.jpg`
-    this.ventas2 = `${configUrlService.getBaseUrl()}/assets/ventas2.png`
+    this.ventas1 = `${configUrlService.getBaseUrl()}/assets/ventas1.png`
+    this.ventas2 = `${configUrlService.getBaseUrl()}/assets/ventas2.jpg`
+    this.ventas3 = `${configUrlService.getBaseUrl()}/assets/ventas3.png`
+    this.ventas4 = `${configUrlService.getBaseUrl()}/assets/ventas4.png`
+    this.ventas5 = `${configUrlService.getBaseUrl()}/assets/ventas5.png`
+    this.ventas6 = `${configUrlService.getBaseUrl()}/assets/ventas6.png`
   }
 
   ngOnInit() {

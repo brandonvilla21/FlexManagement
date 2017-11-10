e34201caa03167e034cdade207d36367355772d0
Brandon Villa <bornofos@gmail.com>
2017-11-09 20:28:55 -0600

    Consulta finished & Movimiento started

--- src/app/help/components/help-content/help-content.component.ts
+++ src/app/help/components/help-content/help-content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigUrlService } from 'app/services/config-url/config.url.service';
 
 @Component({
   selector: 'app-help-content',
@@ -6,11 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./help-content.component.scss']
 })
 export class HelpContentComponent implements    OnInit {
-  public productos1 = 'http://localhost:3000/assets/productos1.png';
-  public productos2 = 'http://localhost:3000/assets/productos2.png';
-  public productos3 = 'http://localhost:3000/assets/productos3.png';
-  public productos4 = 'http://localhost:3000/assets/productos4.png';
-  constructor() {
+  public productos1: string
+  public productos2: string
+  public productos3: string
+  public productos4: string
+  public productos5: string
+  public ventas1: string
+  public ventas2: string
+  constructor( configUrlService: ConfigUrlService) {
+    this.productos1 = `${configUrlService.getBaseUrl()}/assets/productos1.png`
+    this.productos2 = `${configUrlService.getBaseUrl()}/assets/productos2.png`
+    this.productos3 = `${configUrlService.getBaseUrl()}/assets/productos3.png`
+    this.productos4 = `${configUrlService.getBaseUrl()}/assets/productos4.png`
+    this.productos5 = `${configUrlService.getBaseUrl()}/assets/productos5.png`
+    this.ventas1 = `${configUrlService.getBaseUrl()}/assets/ventas1.jpg`
+    this.ventas2 = `${configUrlService.getBaseUrl()}/assets/ventas2.png`
   }
 
   ngOnInit() {
